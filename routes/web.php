fb846c1c02b0c9d4c729f9b5ce28d0610fa75554
liuxing19960713 <13265947403@163.com>
2018-11-11 11:59:38 +0800

    修改多余的代码

--- routes/web.php
+++ routes/web.php
@@ -10,19 +10,17 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-//后台登录和退出
-Route::resource("/adminlogin","Admin\AdminloginController");
-//中间件结合路由组使用
-Route::group(["middleware"=>"adminlogin"],function()
-{
+// //后台登录和退出
+// Route::resource("/adminlogin","Admin\AdminloginController");
+// //中间件结合路由组使用
+// Route::group(["middleware"=>"adminlogin"],function()
+// {
 	
-	//公司模块
-	Route::resource("/company","Admin\CompanyController");
-	//广告
-	Route::resource("/adver","Admin\AdverController");
-});
-//前台首页
-Route::resource("/home","Home\HomeController");
+// 	//公司模块
+// 	Route::resource("/company","Admin\CompanyController");
+// 	//广告
+// 	Route::resource("/adver","Admin\AdverController");
+// });
 
 
 
